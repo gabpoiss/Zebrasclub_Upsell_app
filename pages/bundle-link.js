b9b18ca68765891b03ea7bba788fbdaff9b2f672
Gabriel Poissant <poissantgabriel@gmail.com>
2019-02-21 14:19:42 -0500

    On Branch Master

--- pages/bundle-link.js
+++ pages/bundle-link.js
@@ -14,19 +14,19 @@ import store from "store-js";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 
-const UPDATE_INVENTORY = gql`
-  mutation productVariantUpdate($input: ProductVariantInput!) {
-    productVariantUpdate(input: $input) {
-      product {
-        title
-      }
-      productVariant {
-        id
-        price
-      }
-    }
-  }
-`;
+// const UPDATE_INVENTORY = gql`
+//   mutation productVariantUpdate($input: InventoryAdjustQuantityPayload!) {
+//     productVariantUpdate(input: $input) {
+//       product {
+//         title
+//       }
+//       productVariant {
+//         id
+//         price
+//       }
+//     }
+//   }
+// `;
 
 class BundleLink extends React.Component {
   state = {};
